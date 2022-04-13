8c71576333d33b641f854f8e7d0dfc3a59d65472
Pedrojok01 <pedrojok@pm.me>
2022-04-13 08:37:40 +0700

    fix antd bug with submenu props

--- src/components/Header/components/MenuItems.jsx
+++ src/components/Header/components/MenuItems.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from "react-router";
 import { Menu } from "antd";
 import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
 const { SubMenu } = Menu;
 
 const styles = {
@@ -23,7 +22,7 @@ function MenuItems() {
   return (
     <>
       <Menu mode='horizontal' style={styles.menuItems} defaultSelectedKeys={[pathname]} defaultOpenKeys={["/Home"]}>
-        <SubMenu key='SubMenu1' title='Pack'>
+        <SubMenu key='sub1' title='Pack'>
           {/* <Menu.Item key='batchMinter'>
               <NavLink to='/BatchMinter'>Minter</NavLink>
             </Menu.Item> */}
@@ -47,17 +46,21 @@ function MenuItems() {
           <NavLink to='/Transactions'>Transactions</NavLink>
         </Menu.Item>
 
-        <SubMenu key='SubMenu2' title='Support'>
-          <a href='https://lepricon.gitbook.io/pack-generator/' target='_blank' rel='noopener noreferrer'>
-            <Menu.Item key='documentation'>Documentation / FAQ</Menu.Item>
-          </a>
-          <a
-            href='https://techdev2021.atlassian.net/servicedesk/customer/portal/6'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Menu.Item key='contact'>Contact / Repport an issue</Menu.Item>
-          </a>
+        <SubMenu key='sub2' title='Support'>
+          <Menu.Item key='documentation'>
+            <a href='https://lepricon.gitbook.io/pack-generator/' target='_blank' rel='noopener noreferrer'>
+              Documentation / FAQ
+            </a>
+          </Menu.Item>
+          <Menu.Item key='contact'>
+            <a
+              href='https://techdev2021.atlassian.net/servicedesk/customer/portal/6'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              Contact / Repport an issue
+            </a>
+          </Menu.Item>
         </SubMenu>
       </Menu>
     </>
