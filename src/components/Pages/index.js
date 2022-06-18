1bff99f24d3aa9e3383e9e95a1631ecdcd80d81a
Pedrojok01 <pedrojok@pm.me>
2022-06-18 12:44:49 +0700

    Clean import in App.js

---
+++ src/components/Pages/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "./Home";
+export { default as Marketplace } from "./Marketplace";
+export { default as Minter } from "./Minter";
+export { default as Transactions } from "./Transactions";
+export { default as YourNFTs } from "./YourNFT/YourNFTs";
+export { default as Pack } from "./Pack/Pack";
