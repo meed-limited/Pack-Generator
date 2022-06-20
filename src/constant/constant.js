e2e6c285e5e9a16f753e9b6c0f7611bae70a7b40
Pedrojok01 <pedrojok@pm.me>
2022-06-20 07:34:53 +0700

    constant folder re-added

---
+++ src/constant/constant.js
@@ -0,0 +1,27 @@
+export const CHAINS_WITH_L3P_SUPPORT = ["0x1", "0x38"];
+export const L3P_TOKEN_ADDRESS = "0xdeF1da03061DDd2A5Ef6c59220C135dec623116d";
+export const PACK_LIMIT = 200;
+export const NFTs_PER_PAGE = 100;
+
+/* CONTRACT ADDRESS:
+ ********************/
+
+// Contracts on Ethereum
+export const assemblyAddressEthereum = "";
+export const factoryAddressEthereum = "";
+export const marketAddressEthereum = "";
+
+// Contracts on Polygon
+export const assemblyAddressPolygon = "0xDEBa7Ca66e89C260d793449a8B84AA7f3F749E31";
+export const factoryAddressPolygon = "0x50E2b0280a335C2c0767a112A35E8fbD5ae8F297";
+export const marketAddressPolygon = "0xe0DBcE12b8223A08D7Ba5e2982871b8eD8974077";
+
+// Contracts on BSC
+export const assemblyAddressBSC = "0x80d9a5394ea4B1aa3F1979e47E659F552E85233a";
+export const factoryAddressBSC = "0xB671320777363a4b2695e909B97EC6A340BD3eE4";
+export const marketAddressBSC = "0x5D69a2985fbCfacdC74a1F0A403F75259819A7a5";
+
+// Contracts on Mumbaï
+export const assemblyAddressMumbai = "0x12Cb2fbb6c16f97Da061Af6272d268c4FAfE91eF"; //OK
+export const factoryAddressMumbai = "0xD1EcE8754e16BFE9aA22c47a05Da49351408139F"; //OK
+export const marketAddressMumbai = "0x04a9050317d16F25BB5B25Ae534d6fA330440Dba"; //OK
